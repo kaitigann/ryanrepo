d25e3a6db768131bfc2f93c8c109c7a01ce3d46a
Kaiti Gann <ckljgann@gmail.com>
2024-03-01 18:40:45 -0600

    test deploy

---
+++ new-store/src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import { Inter, Oooh_Baby } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+export default function Home() {
+  return (
+   <>
+   <h1>HELLO WORLD</h1>
+   </>
+  );
+}
